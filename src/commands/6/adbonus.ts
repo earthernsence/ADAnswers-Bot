2a233394d92c24152abb2ad40db715dde30a2859
HexaVault <44529573+HexaVault@users.noreply.github.com>
2024-06-20 22:03:33 +0100

    adbonus is descriptive

--- src/commands/6/adbonus.ts
+++ src/commands/6/adbonus.ts
@@ -11,15 +11,17 @@ export const adbonus: Command = {
 
     // eslint-disable-next-line max-len
     const content: string = `
-Disabled in cel7
-DM: 2
-Memory Gain: 1.5
-Rarity: +5%
-RS: 1.5
-GS: 2
-DT: 2
-EP: min(max(EP^0.01, 1.5), 1e10)
-IP: max(IP^0.01, 2)
+The highest unlocked effect on this list, is the only effect that applies
+Celestial 7: ||Ad Bonus has been Nullified||
+Celestial 6: ||DM: 2||
+Celestial 5: ||Memory Gain: 1.5
+Celestial 3: ||Rarity: +5%||
+Celestial 2: ||RS: 1.5||
+Celestial 1: ||RM: 2x||
+Reality Unlocked in current Reality: ||GS: 2||
+Dilation Unlocked: ||DT: 2||
+10+ Eternities: ||EP: min(max(EP^0.01, 1.5), 1e10)||
+10+ Infintiies: ||IP: max(IP^0.01, 2)||
 AD: 2.
 And no, ad bonus is not coming to steam (you simply can't!)`;
 
