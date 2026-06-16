9f0042d0398611f8bc238bbc535518249a429948
earth <divine.icbm@gmail.com>
2026-06-15 19:24:59 -0500

    fix: confirm bot invite link is correct
    just wanted to make sure the permissions were correct

--- types/Embeds/MetaCustomEmbed.ts
+++ types/Embeds/MetaCustomEmbed.ts
@@ -123,7 +123,7 @@ Expire${this.disabled ? "d" : "s"} ${time(this.expirationTimestamp, TimestampSty
         },
         {
           name: "Invite",
-          value: `If, for whatever reason, you wish to invite me to your server, go to ${link("this link", "https://discord.com/oauth2/authorize?client_id=830197123378053172&permissions=2147748864&integration_type=0&scope=applications.commands+bot")}.`,
+          value: `If, for whatever reason, you wish to invite me to your server, go to ${link("this link", "https://discord.com/oauth2/authorize?client_id=830197123378053172&permissions=277294140416&integration_type=0&scope=bot+applications.commands")}.`,
           inline: true
         },
         {
